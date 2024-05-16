7fe7f41be83a11d3a18ff13f448c2ebcac29f2b6
muXxer <git@muxxer.de>
2024-05-16 19:04:59 +0200

    Reduce slot times to 5s

--- tools/docker-network/tests/dockertestframework/options.go
+++ tools/docker-network/tests/dockertestframework/options.go
@@ -22,22 +22,22 @@ var DefaultProtocolParametersOptions = []options.Option[iotago.V3ProtocolParamet
 // This would cause the tests to force commit all slots at the beginning, because the genesis time is far in the past.
 var ShortSlotsAndEpochsProtocolParametersOptionsFunc = func() []options.Option[iotago.V3ProtocolParameters] {
 	return []options.Option[iotago.V3ProtocolParameters]{
-	iotago.WithStorageOptions(100, 1, 10, 100, 100, 100),
-	iotago.WithWorkScoreOptions(500, 110_000, 7_500, 40_000, 90_000, 50_000, 40_000, 70_000, 5_000, 15_000),
-	//iotago.WithTimeProviderOptions(0, time.Now().Unix(), 10, 13),
-	iotago.WithTimeProviderOptions(5, time.Now().Unix(), 10, 3),
-	//iotago.WithLivenessOptions(15, 30, 10, 20, 60),
-	iotago.WithLivenessOptions(10, 10, 2, 4, 5),
-	iotago.WithSupplyOptions(1813620509061365, 63, 1, 17, 32, 21, 70),
-	//iotago.WithCongestionControlOptions(1, 1, 1, 400_000_000, 250_000_000, 50_000_000, 1000, 100),
-	iotago.WithCongestionControlOptions(1, 1, 1, 200_000_000, 125_000_000, 50_000_000, 1000, 100),
-	iotago.WithStakingOptions(10, 10, 10),
-	iotago.WithVersionSignalingOptions(7, 5, 7),
-	//iotago.WithRewardsOptions(8, 11, 2, 384),
-	iotago.WithRewardsOptions(8, 10, 2, 384),
-	//iotago.WithTargetCommitteeSize(32),
-	iotago.WithTargetCommitteeSize(4),
-	iotago.WithChainSwitchingThreshold(3),
+		iotago.WithStorageOptions(100, 1, 10, 100, 100, 100),
+		iotago.WithWorkScoreOptions(500, 110_000, 7_500, 40_000, 90_000, 50_000, 40_000, 70_000, 5_000, 15_000),
+		//iotago.WithTimeProviderOptions(0, time.Now().Unix(), 10, 13),
+		iotago.WithTimeProviderOptions(5, time.Now().Unix(), 5, 3),
+		//iotago.WithLivenessOptions(15, 30, 10, 20, 60),
+		iotago.WithLivenessOptions(5, 5, 2, 4, 5),
+		iotago.WithSupplyOptions(1813620509061365, 63, 1, 17, 32, 21, 70),
+		//iotago.WithCongestionControlOptions(1, 1, 1, 400_000_000, 250_000_000, 50_000_000, 1000, 100),
+		iotago.WithCongestionControlOptions(1, 1, 1, 200_000_000, 125_000_000, 50_000_000, 1000, 100),
+		iotago.WithStakingOptions(10, 10, 10),
+		iotago.WithVersionSignalingOptions(7, 5, 7),
+		//iotago.WithRewardsOptions(8, 11, 2, 384),
+		iotago.WithRewardsOptions(8, 10, 2, 384),
+		//iotago.WithTargetCommitteeSize(32),
+		iotago.WithTargetCommitteeSize(4),
+		iotago.WithChainSwitchingThreshold(3),
 	}
 }
 
